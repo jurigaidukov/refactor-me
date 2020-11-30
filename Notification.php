408476ade9457eae38aacc08f7514e6963c2a62a
Hleb Savchenko <41167859+birdsaynevermore@users.noreply.github.com>
2020-11-30 20:13:10 +0200

    Update Notification.php

--- Notification.php
+++ Notification.php
@@ -5,16 +5,18 @@ class Notification
     public $adminEmail;
 
     public function init($adminEmail) {
-        assert(strlen($adminEmail) > 5);
+        
+        if($adminEmail > 5) {
+            
         $this->adminEmail = $adminEmail;
-    }
+        }
 
     public function sendMessageTo($app, $title, $message) {
         if (!empty($title) && !empty($message)) {
             if ($app == 'discord') {
                 $dis = new DiscordApi;
                 $discordChannel = 'https://discordapp.com/api/webhooks/xxx';
-                $disc_message = '#'.$title.'# ' . strip_tags($message);
+                $disc_message = '#'.$title.'#'. strip_tags($message);
                 $dis->sendMessage($discordChannel, $disc_message);
             } else {
                 mail($this->adminEmail, $title, $message);
